04674ab0a98b14dfcd26c15356b14c42cebdbc8d
Quentin-27 Quentin27 <quentin.mateus@ynov.com>
2026-04-03 17:00:02 +0200

    [ADD]-Ajout d'un server qui lance un html , et qui le main appèle le fichier serveur (système de module utilisé : ES module)

--- Main.js
+++ Main.js
@@ -1,9 +1,13 @@
-import { Game } from './game.js';
-import {comp} from './cpu.js'
+import './server/Server.js'   
+
+import { Game } from './game.js'
+import { comp } from './cpu.js'
+
 const gaming = new Game()
+
 gaming.createjoueur(4)
 gaming.start()
+
 while (gaming.victory == false){
     comp(gaming)
 }
-
